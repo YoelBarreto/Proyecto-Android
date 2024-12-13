1195563d449cbc9bbfc6c63f001ac33e90b23609
Yoel <yobelb8@gmail.com>
2024-12-13 12:02:09 +0000

    Añadido archivo base de datos (Incompleto)

---
+++ app/src/main/java/model/AppDataBase.kt
@@ -0,0 +1,28 @@
+package model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class, TypeTask::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+    abstract fun typeTaskDao(): TypeTaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "care-animal_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
