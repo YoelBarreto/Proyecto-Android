edc4fbf8d3282f85897784c73c0eb38e4b77c86a
Yoel <yobelb8@gmail.com>
2024-12-16 13:43:55 +0000

    Preparado Main sin ViewModel

--- app/src/main/java/com/yoel/proyecto_android/MainActivity.kt
+++ app/src/main/java/com/yoel/proyecto_android/MainActivity.kt
@@ -7,21 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import model.AppDatabase
+import view.AnimalScreen
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var database: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        database = AppDatabase.getDatabase(this)
         setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Greeting(
-                    name = "Android",
-                    modifier = Modifier.padding(innerPadding)
-                )
+            Surface(modifier = Modifier.fillMaxSize()) {
+                AnimalScreen(database)
             }
         }
     }
