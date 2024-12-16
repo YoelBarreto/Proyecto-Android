341b442ba20c2a3b028e8960aaff5938a3b5e3fa
Yoel <yobelb8@gmail.com>
2024-12-16 13:02:50 +0000

    Cambio de método de relacion

--- app/src/main/java/model/Animal.kt
+++ app/src/main/java/model/Animal.kt
@@ -7,21 +7,10 @@ import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "animals",
-    foreignKeys = [
-        ForeignKey(
-            entity = Ubication::class,
-            parentColumns = ["ubicationId"],
-            childColumns = ["ubicationId"],
-            onDelete = ForeignKey.CASCADE // Si se elimina la ubicación, elimina el animal.
-        )
-    ],
-    indices = [Index(value = ["ubicacionId"])] // Crear índice para mejorar el rendimiento.
 )
 data class Animal(
     @PrimaryKey(autoGenerate = true)
     val animalId: Int = 0,
     val name: String,
     val specie: String,
-
-    val ubicationId: Int
 )
