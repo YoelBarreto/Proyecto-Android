2aaf902ae777acef3fa8413a46c366a66ad6a136
Yoel <yobelb8@gmail.com>
2024-12-16 11:51:24 +0000

    Commit para dejar blanco Main

--- app/src/main/java/com/yoel/proyecto_android/MainActivity.kt
+++ app/src/main/java/com/yoel/proyecto_android/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +25,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
