b73dea226ae595ca5b01262fef051f4609a57a42
Yoel <yobelb8@gmail.com>
2024-12-16 13:37:09 +0000

    Creada clave foranea en Vacuna para borrar en cascada

--- app/src/main/java/model/entities/Vacinne.kt
+++ app/src/main/java/model/entities/Vacinne.kt
@@ -1,10 +1,19 @@
 package model.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "vacinnes",
+    foreignKeys = [
+        ForeignKey(
+            entity = Animal::class,
+            parentColumns = ["animalId"],
+            childColumns = ["animalId"],
+            onDelete = ForeignKey.CASCADE // Elimina las vacunas relacionadas cuando se borra el animal
+        )
+    ]
 )
 data class Vacinne(
     @PrimaryKey(autoGenerate = true)
