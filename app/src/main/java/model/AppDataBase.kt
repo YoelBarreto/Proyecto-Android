47cfe15a9e34c59180bef38dbacb0e611883aca5
Yoel <yobelb8@gmail.com>
2024-12-16 12:27:59 +0000

    Eliminada tabla + Dao + Screen por abundantes problemas

--- app/src/main/java/model/AppDataBase.kt
+++ app/src/main/java/model/AppDataBase.kt
@@ -5,10 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Task::class, TypeTask::class], version = 1)
+@Database(entities = [Animal::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun taskDao(): TaskDao
-    abstract fun typeTaskDao(): TypeTaskDao
+    abstract fun animalDao(): Animal
 
     companion object {
         @Volatile
