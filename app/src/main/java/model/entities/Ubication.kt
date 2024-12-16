7da9d413926cb407246dd8652f20e1f5d37bc081
Yoel <yobelb8@gmail.com>
2024-12-16 13:36:58 +0000

    Creada clave foranea en Ubicacion para borrar en cascada

--- app/src/main/java/model/entities/Ubication.kt
+++ app/src/main/java/model/entities/Ubication.kt
@@ -1,9 +1,20 @@
 package model.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "ubications")
+@Entity(
+    tableName = "ubications",
+    foreignKeys = [
+        ForeignKey(
+            entity = Animal::class,
+            parentColumns = ["animalId"],
+            childColumns = ["animalId"],
+            onDelete = ForeignKey.CASCADE // Elimina las ubicaciones relacionadas cuando se borra el animal
+        )
+    ]
+)
 data class Ubication(
     @PrimaryKey(autoGenerate = true)
     val ubicationId: Int = 0,
