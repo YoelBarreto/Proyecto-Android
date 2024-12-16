35274497b8ae7f905d0f812197610f004c19280f
Yoel <yobelb8@gmail.com>
2024-12-16 12:46:32 +0000

    Creada la clase Vacuna

---
+++ app/src/main/java/model/Vacinne.kt
@@ -0,0 +1,26 @@
+package model
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "vacinnes",
+    foreignKeys = [
+        ForeignKey(
+            entity = Animal::class,
+            parentColumns = ["animalId"],
+            childColumns = ["animalId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["animalId"])]
+)
+data class Vacinne(
+    @PrimaryKey(autoGenerate = true)
+    val vacinneId: Int = 0,
+    val name: String,
+    val date: String,
+    val animalId: Int // Clave foránea hacia Animal.
+)
