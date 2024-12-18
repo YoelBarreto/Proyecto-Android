4728d1ccf4d99956fed829633379ebf47817d7bf
Yoel <yobelb8@gmail.com>
2024-12-18 08:34:43 +0000

    Aplicado cambio en pantalla

--- app/src/main/java/com/yoel/proyecto_android/MainActivity.kt
+++ app/src/main/java/com/yoel/proyecto_android/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import model.AppDatabase
 import view.AnimalScreen
 import model.tests.TestDao
@@ -18,7 +25,6 @@ import model.tests.TestDao
 
 class MainActivity : ComponentActivity() {
     private lateinit var database: AppDatabase
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,17 +32,27 @@ class MainActivity : ComponentActivity() {
         val testDao = TestDao(applicationContext)
         setContent {
             Surface(modifier = Modifier.fillMaxSize()) {
-                AnimalScreen(database)
+//                AnimalScreen(database)
+                // Al no tener interfaz, por lo menos tener algo en pantalla
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFF2d88c8)),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "N/A", fontSize = 46.sp, fontWeight = FontWeight.Bold)
+                }
             }
 
 //            // Sin interfáz
-//            // Insertar datos
+//            // Insertar los datos mediante CompleteAnimal, insertando todos los datos.
 //            testDao.testInsert()
 //
-//            // Actualizar datos
+//            // Actualizar/Cambiar los datos mediante CompleteAnimal.
 //            testDao.testUpdate()
 //
-//            // Eliminar datos
+//            // Eliminar los datos mediante ID
 //            testDao.testDelete()
         }
     }
